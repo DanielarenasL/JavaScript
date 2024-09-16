ff72f801ece6533f8f5e26dc856a34b7a17c59b2
DanielarenasL <daniel2004al14@gmail.com>
2024-09-16 14:48:17 -0500

    CRUD completo

--- api.js
+++ api.js
@@ -1,6 +1,9 @@
-//botones
+//Api
 const autorizacion = 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsImVtYWlsIjoiZGFuaWVsLmFyZW5hczFAdXRwLmVkdS5jbyIsImlhdCI6MTcyNjQ1NjkwMiwiZXhwIjoxNzQzNzM2OTAyfQ.Ox67UtgDCJSP5HJxb8L5Hafs_9nS13exH6L2GJgE334'
 const url_api = 'https://fake-api-vq1l.onrender.com/posts'
+
+//botones
+const agregar = document.getElementById('crear');
 //listar
 fetch(url_api,{
     headers: {
@@ -22,8 +25,8 @@ fetch(url_api,{
                 <p>${product.value}</p>
                 <p>categoria: ${product.category_id}</p>
                 <div>
-                    <button onclick='borrar(${product.id})'>Eliminar</button>
-                    <button id='editar'>Editar</button>
+                    <button onclick='borrar(${product.id})' id='eliminar'>Eliminar</button>
+                    <button onclick='editar(${product.id})' id='editar'>Editar</button>
                 </div>
             </div>
         </li>`;
@@ -31,8 +34,77 @@ fetch(url_api,{
         
 });
         
+//agregar
+agregar.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let value = parseInt(document.getElementById('value').value);
+    let category = parseInt(document.getElementById('category_id').value);
+    let images = [document.getElementById('images').value];
+    let hola = ['hola', 'adios']
+    console.log(typeof(title), typeof(description), typeof(value), typeof(category), typeof(images), typeof(hola));
+
+    fetch(url_api, {
+        method: 'POST',
+        headers: {
+            "Authorization": autorizacion,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: title,
+            description: description,
+            value: value,
+            category_id: category,
+            images: images
+        })
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+        if(response) {
+            alert('Producto creado');
+        }
+        location.reload();
+    })
+})
+
+//editar
+function editar(id){
+
+    let title = prompt("Digite el nuevo titulo")
+    let description = prompt("Digite la nueva descripcion")
+    let value = parseInt(prompt("Digite el nuevo valor"))
+    let category = parseInt(prompt("Digite la nueva categoria"))
+    let images = [prompt("Digite la nueva imagen")]
+
+    fetch(url_api + '/' + id, {
+        method: 'PATCH',
+        headers: {
+            "Authorization": autorizacion,
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            title: title,
+            description: description,
+            value: value,
+            category_id: category,
+            images: images
+        })
+    })
+    .then(response => response.json())
+    .then(response => {
+        console.log(response);
+        if(response){
+            alert('Producto actualizado');
+        }
+        location.reload();
+    })
+}
 
 
+//borrar
 function borrar(id){
     fetch(url_api + '/' + id, {
         method: 'DELETE',
@@ -48,5 +120,4 @@ function borrar(id){
         }
         location.reload();
     })
-    
 }
